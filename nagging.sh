d5b3f5a993601767fdcd2a7da0bca038ee690f9a
许向 <x2x4com@gmail.com>
2019-01-15 13:46:35 +0800

    update

--- nagging.sh
+++ nagging.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-s=""
+base="${HOME}/lattice_painter/"
+
+year=$(date +%Y)
+
+month=$(date +%m)
+
+day=$(date +%d)
+
+hour=$(date +%H)
+
+min=$(date +%M)
+
+[ ! -f $base ] && echo "${base} not existed" && exit 127
+
+t=${base}/rubbish/${year}/${month}
+
+[ ! -d "$t" ] && mkdir -p $t
+
+echo "balabala ${hour}-${min}" >> $t/$hour
+
+cd $base
+git add . && git commit -m "update ${year}/${month}/${day} - ${hour}:${min}" && git push
